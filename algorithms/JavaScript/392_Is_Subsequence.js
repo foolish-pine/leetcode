77ec5233cb3c910aab66e90c2f664596174b22b4
MatsudaTakuya <matsuda.takuya41@gmail.com>
2020-09-19 15:53:12 +0900

    commit

---
+++ algorithms/JavaScript/392_Is_Subsequence.js
@@ -0,0 +1,10 @@
+const isSubsequence = (s, t) => {
+  for (let i = 0; i < s.length; i++) {
+    if (t.includes(s[i])) {
+      t = t.slice(t.indexOf(s[i]) + 1);
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
